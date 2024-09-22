12f0fccd1e096fde756b65b9cd7304ec64b937c0
Umut Seyhan <umutseyhan115@gmail.com>
2024-09-22 17:11:35 +0300

    .

---
+++ lib/presentation/intro/pages/get_started.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:spotify/core/configs/assets/app_images.dart';
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.fill, image: AssetImage(AppImages.introBG))),
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.15),
+          )
+        ],
+      ),
+    );
+  }
+}
